1d17b7caf4a1fe39821366a50f0d7ea30e868db4
ThinkFree <t4m215@gmail.com>
2026-09-03 06:20:15 +0800

    chore: initial commit

---
+++ ThinkStudio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThinkStudio
+//
+//  Created by ThinkFree on 03/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThinkStudio/ThinkStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkStudioApp.swift
+//  ThinkStudio
+//
+//  Created by ThinkFree on 03/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ThinkStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
